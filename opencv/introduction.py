4aba5f0c6e26139e192946e9736c25ccec4d9136
Sadman Kabir Soumik <sadman.soumik@chowagiken.co.jp>
2019-11-04 18:23:21 +0600

    opencv intro

--- opencv/introduction.py
+++ opencv/introduction.py
@@ -1,5 +1,10 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Load a color image in grayscale
+img = cv.imread('soumikfunk.jpeg', 0)
 
+cv.imshow('soumik', img)
+cv.waitKey(0)
+cv.destroyAllWindows()
